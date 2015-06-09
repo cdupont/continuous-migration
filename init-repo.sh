065b4dbc92422ca28fa64a1ec9eaaae0e844a4fe
Corentin Dupont <cdupont@create-net.org>
2015-06-09 13:58:08 +0200

    init repo script

--- init-repo.sh
+++ init-repo.sh
@@ -1,14 +1,16 @@
 git tag -d V0 V1 V2
 
-echo "module Types where \ndata MyType = MyType Int deriving (Show)\n" > Types.hs
+myFile=src/Types.hs
 
-git add src/Types.hs; git commit -n "V0"; git tag V0
+printf "module Types where \ndata MyType = MyType Int deriving (Show)\n" >> $myFile
 
-echo "module Types where \ndata MyType = MyType Int Int deriving (Show)\n" > Types.hs
+git add $myFile; git commit -m "V0"; git tag V0
 
-git add src/Types.hs; git commit -n "V1"; git tag V1
+printf "module Types where \ndata MyType = MyType Int Int deriving (Show)\n" > $myFile
 
-echo "module Types where \ndata MyType = MyType Int Int Int deriving (Show)\n" > Types.hs
+git add $myFile; git commit -m "V1"; git tag V1
 
-git add src/Types.hs; git commit -n "V2"; git tag V2
+printf "module Types where \ndata MyType = MyType Int Int Int deriving (Show)\n" > $myFile
+
+git add $myFile; git commit -m "V2"; git tag V2
 
